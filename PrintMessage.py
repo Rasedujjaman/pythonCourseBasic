1ec9dcc9adc14bd1c8ae7eda3f0be17df7d2c075
Rasedujjaman <51413817+Rasedujjaman@users.noreply.github.com>
2021-09-03 11:02:21 +0200

    initial commit

---
+++ PrintMessage.py
@@ -0,0 +1,17 @@
+# Interpreted with python 3.7.0
+# In this python program we will show different ways to print string
+
+# plain string
+print("Hello Universe!")
+
+
+
+# String concatenation
+your_name = "Karim"  # string variable
+your_age = 25  # integer type variable
+print("Your name is " + your_name + " and your are " + str(your_age))
+print(f"Your name is {your_name} and your are {your_age}") # Better way
+
+
+
+ 
---
+++ VariablesType.py
@@ -0,0 +1,30 @@
+# This python code is interpreted with python 3.7.0
+
+# In this python code we will show how to display different variable types
+
+### Important note: In python the variables are dynamic type meaning 
+### it is not necessary to declear the vairalbe type like in C++
+
+
+your_name = "Karim" # it is a stirng variable
+your_age = 30   # it is an integer type variable
+your_cgpa = 3.5 # it is a float type variable
+your_grade = 'B' # it is also a string type
+
+# The equivalent variable in C++ is declared as follows 
+#  string your_name = "Karrim";
+#  int your_age = 30;
+
+
+print(type(your_name))
+print(type(your_age))
+print(type(your_cgpa))
+print(type(your_grade))
+
+
+### Note: From the above output we see that, In python everyting is considered as object
+
+### <class, 'int'>
+### < class, 'str'>
+### <class, 'float'>
+### <class, 'str'>
